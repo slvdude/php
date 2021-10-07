ad0b132982dbe44535266eedf6fb8d2218b9dfba
sergey <slavdudeight@gmail.com>
2021-10-08 00:35:25 +0400

    deleting functionality doesn't work

---
+++ deleteTodo.php
@@ -0,0 +1,7 @@
+<?php
+    $todos = json_decode($_COOKIE['todos'], true) ?? [];
+    $todoTitle = $_POST['todo_title'];
+    unset($todos[$todoTitle]);
+    setcookie('todos', json_encode($todos));
+    header('Location: index.php');
+?>
