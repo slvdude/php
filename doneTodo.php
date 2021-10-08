2fa328d984fe09dc40ed6d976235548777343659
sergey <slavdudeight@gmail.com>
2021-10-08 20:35:26 +0400

    ALMOST...

--- doneTodo.php
+++ doneTodo.php
@@ -1,7 +1,6 @@
 <?php
     $todos = json_decode($_COOKIE['todos'], true) ?? [];
     
-
     function doneAllTodos($arr) {
         foreach($arr as $key1 => $todo) {
             foreach($todo as $key2 => $el) {
@@ -14,14 +13,23 @@
         header('Location: index.php');
     }
 
-    function doneSingularTodo() {
+    function doneSingularTodo($arr) {
         $todoTitle = $_POST['done_todo'];
-        if($todos[$todoTitle]['done'] == false) {
-            $todos[$todoTitle]['done'] = true;
-        } else {
-            $todos[$todoTitle]['done'] = false;  
+        foreach($arr as $key => $todo) {
+            if($key == $todoTitle) {
+                $rightIndex = $key;
+                foreach($todo as $key1 => $el) {
+                    if($key1 == 'done') {
+                        if($arr[$key][$key1] === false) {
+                            $arr[$key][$key1] = true;
+                        } else {
+                            $arr[$key][$key1] = false; 
+                        }
+                    }
+                }
+            }
         }
-        setcookie('todos', json_encode($todos));
+        setcookie('todos', json_encode($arr));
         header('Location: index.php');
     }
 
@@ -30,7 +38,7 @@
     }  
 
     if(isset($_POST['done_todo'])) {
-        doneSingularTodo();
+        doneSingularTodo($todos);
     }
     
 ?>
